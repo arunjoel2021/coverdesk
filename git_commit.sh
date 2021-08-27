e90f13587b9525733791b5ea97040c9ab0f8497d
Arun Joel <arunjoel2021@gmail.com>
2021-08-27 13:13:41 +0000

    + New

--- git_commit.sh
+++ git_commit.sh
@@ -3,8 +3,8 @@ git init
 git config --global user.email "arunjoel2021@gmail.com"
 git config --global user.name "Arun Joel"
 git remote set-url origin https://github.com/arunjoel2021/coverdesk.git
-git add .
+git add -A
 echo "please say what changes were done, Eg : Updated download file"
 read -r comment 
 git commit -m "$(date +%Y-%m-%d %H:%M:%S) + $comment"
-git push -u origin main
+git push -u origin master
